1085d489dbcc0dea32c53dcf4cdb0b1a2e3f9eb1
yuvraj <143809934+yuvrajsssingh@users.noreply.github.com>
2023-11-29 09:28:33 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,39 @@
+# #case=1 (no argument no return)
+
+# def add():
+#     a=5
+#     b=4
+#     c=a+b
+#     print("add=",c)
+# add()
+
+# #case=2(with argumenet no return)
+
+# def add(a,b):
+#     c=a+b
+#     print("add=",c)
+# a=4
+# b=5
+# add(a,b)
+
+# #case=3(no argument with return )
+
+# def add():
+#     a=4
+#     b=5
+#     c=a+b
+#     return c
+# x=add()
+# print("addition=",x)
+
+# #case=4(with argument with return)
+
+# def add(a,b):
+#     c=a+b
+#     return c
+# a=3
+# b=7
+# p=add(a,b)
+# print("addition=",p)
+
+
